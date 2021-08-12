e6c29da40c9457cdf33b1487a4765e097c991a08
trishamarchena <galadrielvasquez@gmail.com>
2021-08-12 18:44:30 -0400

    created the tables and app

---
+++ db/migrate/20210812220844_create_plants.rb
@@ -0,0 +1,14 @@
+class CreatePlants < ActiveRecord::Migration[6.1]
+  def change
+    create_table :plants do |t|
+      t.string :name
+      t.references :user, null: false, foreign_key: true
+      t.string :watering_input
+      t.string :sunlight_input
+      t.string :other_input
+      t.string :image_url
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20210812221716_create_plant_cares.rb
@@ -0,0 +1,13 @@
+class CreatePlantCares < ActiveRecord::Migration[6.1]
+  def change
+    create_table :plant_cares do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :plant, null: false, foreign_key: true
+      t.date :date_edit
+      t.string :notes
+      t.date :date_acq
+
+      t.timestamps
+    end
+  end
+end
