8409f89aa77d6936f59f70ca8f198d6b17f15f88
trishamarchena <galadrielvasquez@gmail.com>
2021-10-17 22:44:12 -0400

    switched files

--- config/initializers/datadog-tracer.rb
+++ config/initializers/datadog-tracer.rb
@@ -2,4 +2,12 @@
 Datadog.configure do |c|
   c.env = 'envdd'
   c.service = 'servicedd'
+  c.tracer sampler: Datadog::PrioritySampler.new(
+    post_sampler: Datadog::Sampling::RuleSampler.new(
+        [
+            # Sample all 'servicedd' traces at 80.00%:
+            Datadog::Sampling::SimpleRule.new(service: 'servicedd', sample_rate: 0.8000)
+        ]
+    )
+    )
 end
--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -3,13 +3,5 @@ require 'ddtrace'
 Datadog.configure do |c|
   # This will activate auto-instrumentation for Rais
   c.use :rails, log_injection: true
-  c.tracer sampler: Datadog::PrioritySampler.new(
-    post_sampler: Datadog::Sampling::RuleSampler.new(
-        [
-            # Sample all 'servicedd' traces at 100.00%:
-            Datadog::Sampling::SimpleRule.new(service: 'servicedd', sample_rate: 1.000)
-        ]
-    )
-    )
 
 end
