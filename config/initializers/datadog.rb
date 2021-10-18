e9e7a02f4a7ee99485fd0f9c33db916aae153c00
trishamarchena <galadrielvasquez@gmail.com>
2021-10-17 21:22:53 -0400

    adjusted sampler to backedn

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -6,8 +6,8 @@ Datadog.configure do |c|
   c.tracer sampler: Datadog::PrioritySampler.new(
     post_sampler: Datadog::Sampling::RuleSampler.new(
         [
-            # Sample all 'servicedd' traces at 80.00%:
-            Datadog::Sampling::SimpleRule.new(service: 'servicedd', sample_rate: 0.8000)
+            # Sample all 'servicedd' traces at 100.00%:
+            Datadog::Sampling::SimpleRule.new(service: 'servicedd', sample_rate: 1.000)
         ]
     )
     )
