cb251c5f01cfd3433cda7cdf0d0f58330ae07cf1
trishamarchena <galadrielvasquez@gmail.com>
2021-10-17 20:47:37 -0400

    added sampler to backedn

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -3,4 +3,13 @@ require 'ddtrace'
 Datadog.configure do |c|
   # This will activate auto-instrumentation for Rails
   c.use :rails, log_injection: true
+  c.tracer sampler: Datadog::PrioritySampler.new(
+    post_sampler: Datadog::Sampling::RuleSampler.new(
+        [
+            # Sample all 'servicedd' traces at 80.00%:
+            Datadog::Sampling::SimpleRule.new(service: 'servicedd', sample_rate: 0.8000)
+        ]
+    )
+    )
+
 end
